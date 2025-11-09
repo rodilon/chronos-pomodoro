5ae5a1b3ff7adb5db3a8dc7dc9e545467c955ebe
rodilon <renanodilon@gmail.com>
2025-11-09 17:04:59 -0300

    feat: cria o componente de cycles

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
@@ -30,7 +31,6 @@ export function App() {
               id='meuInput'
               type='string'
               placeholder='Digite algo'
-              disabled
               defaultValue={'Valor preenchido'}
             />
           </div>
@@ -40,8 +40,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className='formRow'>
