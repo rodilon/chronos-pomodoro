e9c2ddaf5cb2d9d06a0d4b14de7b5eab1557dc59
rodilon <renanodilon@gmail.com>
2025-11-10 19:54:58 -0300

    feat: cria componente de footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { CircleStopIcon, PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -49,6 +50,10 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
