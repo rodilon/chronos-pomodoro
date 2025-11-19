d018f6d54b8cb5f06de055298b74b19bd0c32363
rodilon <renanodilon@gmail.com>
2025-11-19 18:05:41 -0300

    feat: implementa criação de nova tarefa com validação e gerenciamento de estado no contexto

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,14 +3,49 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../context/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
   // const [taskName, setTaskName] = useState(''); Forma nao controlada. Componentes atualizam toda vez que um evento é lancado. Ex: digitar cada letra no onChange
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('DEU CERTO!');
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // conferir
+        secondsRemaining, // conferir
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
