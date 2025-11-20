34f9ebbea64380e1488e44935ae0b52b5b808cad
rodilon <renanodilon@gmail.com>
2025-11-20 09:38:12 -0300

    feat: adiciona renderização condicional do componente Cycles no formulário principal

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -72,9 +72,11 @@ export function MainForm() {
         <p>Proximo ciclo 25min</p>
       </div>
 
-      <div className='formRow'>
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
 
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} />
